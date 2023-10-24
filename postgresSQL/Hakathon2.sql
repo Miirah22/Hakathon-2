5e84e048a613874b40b0baeca8dec4b8b40c3429
Miirah22 <139708617+Miirah22@users.noreply.github.com>
2023-10-24 13:18:54 +0400

    add files

---
+++ postgresSQL/Hakathon2.sql
@@ -0,0 +1,12 @@
+--INSERT INTO "empForm"
+--VALUES (30,'John Smith', 35, 'NewYork', 'johnsmith@gmail.com', 54692336, 'officer', '2023-03-05'  )
+
+--SELECT * FROM "empForm"
+
+--DELETE FROM "empForm" WHERE "empNo" = 28
+
+--SELECT * FROM "empForm"
+
+--UPDATE "empForm" set "fname" = 'johnsmith' WHERE "empNo" = 27 
+
+SELECT * FROM "empForm"
